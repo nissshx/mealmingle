02b951bb2bcf7e8885fd176505452be74fb6bf06
Nishant Kumar <156506194+nissshx@users.noreply.github.com>
2024-02-17 18:58:12 +0530

    added mongodb configuration also added <Image> in home

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import Image from 'next/image'
 
-const page = () => {
+import connectDB from '../db'
+connectDB();
+export default function Page() {
   return (
-    <div>Home</div>
+    <Image
+      src="https://images.unsplash.com/photo-1682687220198-88e9bdea9931?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
   )
 }
-
-export default page
