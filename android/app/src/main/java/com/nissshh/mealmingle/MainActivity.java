fa456f27ee5665d6ca761c8d32c06ab8ff8f46f1
Nishant Kumar <156506194+nissshx@users.noreply.github.com>
2024-02-17 18:16:48 +0530

    created home screen,added login,etc,. before mongodb

---
+++ android/app/src/main/java/com/nissshh/mealmingle/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nissshh.mealmingle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nissshh.mealmingle',
+  appName: 'mealmingle',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
